704d91a47d396c46125d018e389a7becb7ebfd4d
Dannissimo20 <Denicha_d_89@mail.ru>
2024-07-31 11:47:18 +0300

    Готов выбор услуги, времени, даты, сотрудника, остался summary

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,22 +6,33 @@ export class AppController {
   constructor(private appService: AppService) {
   }
 
+  private chosen_services :any[] = []
+  private services :any[] = []
+  private employees :any[] = []
+  private chosen_employee :any
+  private chosen_day :any
+  private chosen_time :any
+
   @Get()
   @HttpCode(222)
   @Render('index')
   async root() {
     const categories = await this.appService.getCategories()
-    const services = await this.appService.getServices()
+    this.services = await this.appService.getServices()
+    this.services.forEach(service => {
+      if (!service.comment){
+        service.comment = "Без комментариев"
+      }
+    })
     const filial = await this.appService.getFilialInfo()
     let address = ""
-    let address_array = filial.address.split(';')
-    address_array.forEach(word => {
+    filial.address.split(';').forEach(word => {
       address += word + ', '
     })
     address = address.slice(0, -2)
     return {
       categories: JSON.stringify(categories),
-      services: JSON.stringify(services),
+      services: JSON.stringify(this.services),
       filial_name: filial.name,
       filial_address: address,
       filial_cover: filial.coverphoto,
@@ -29,28 +40,61 @@ export class AppController {
     };
   }
 
-  @Get('/service')
-  @HttpCode(222)
-  @Render('service')
-  async service() {
-    const categories = await this.appService.getCategories()
-    const services = await this.appService.getServices()
-    const filial = await this.appService.getFilialInfo()
-    let address = ""
-    let address_array = filial.address.split(';')
-    address_array.forEach(word => {
-      address += word + ', '
-    })
-    address = address.slice(0, -2)
-    return {
-      categories: JSON.stringify(categories),
-      services: JSON.stringify(services),
-      filial_name: filial.name,
-      filial_address: address,
-      filial_cover: filial.coverphoto,
-      filial_logo: filial.avatar
-    };
+  @Get('choose_service/:id')
+  @Redirect('/booking', 302)
+  async choose_service(@Param() params: any) {
+    this.chosen_services = []
+    this.chosen_services.push(this.services.find(service => service.id === params.id))
+    this.employees = await this.appService.getUsers(params.id)
+    if (this.employees.length === 1) {
+        this.chosen_employee = this.employees[0]
+    }
   }
+
+  @Get('/exit')
+  @Redirect('/', 302)
+  async exit() {
+    this.chosen_services = []
+  }
+
+  @Get('/booking')
+  @Render('booking')
+  async booking() {
+    if (this.chosen_employee){
+      let dates = await this.appService.getDates(this.chosen_employee.id)
+      return {
+        chosen_services: JSON.stringify(this.chosen_services),
+        days: JSON.stringify(dates),
+        employee_fio: this.chosen_employee.fio,
+        time_start: 12,
+        time_end: 16,
+        interval: 30
+      }
+    }
+    else
+      return {
+        chosen_services: JSON.stringify(this.chosen_services),
+        employee_fio: "Любой сотрудник",
+      }
+  }
+
+  @Post('/booking/choose_day')
+  async booking_day(@Body() body: any){
+    this.chosen_day = new Date(new Date(body.date).getFullYear(), new Date(body.date).getMonth(), new Date(body.date).getDate())
+    const times = await this.appService.getTimes(this.chosen_employee.id, body.date)
+    return {
+        message: "Success",
+        time_start: parseInt(times.employee.work_start.slice(0, 2)),
+        time_end: parseInt(times.employee.work_end.slice(0, 2)),
+        interval: 30
+      }
+  }
+
+  @Post('/booking/choose_time')
+  async booking_time(@Body() body: any){
+    this.chosen_time = body.time+":00"
+  }
+
 
   @Get('/summary')
   @HttpCode(222)
@@ -75,16 +119,33 @@ export class AppController {
     };
   }
 
-  @Get('/booking')
-  @Render('booking')
-  booking() {
-    return
-  }
-
   @Get('/employee')
   @Render('employee')
-  employee(){
-    return
+  employee_page(){
+    return {employees: JSON.stringify(this.employees)}
+  }
+
+  @Get('/service')
+  @HttpCode(222)
+  @Render('service')
+  async service() {
+    const categories = await this.appService.getCategories()
+    const services = await this.appService.getServices()
+    const filial = await this.appService.getFilialInfo()
+    let address = ""
+    let address_array = filial.address.split(';')
+    address_array.forEach(word => {
+      address += word + ', '
+    })
+    address = address.slice(0, -2)
+    return {
+      categories: JSON.stringify(categories),
+      services: JSON.stringify(services),
+      filial_name: filial.name,
+      filial_address: address,
+      filial_cover: filial.coverphoto,
+      filial_logo: filial.avatar
+    };
   }
 
   @Post('spam')
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,89 @@
 import {HttpService} from '@nestjs/axios';
-import {Injectable} from '@nestjs/common';
+import {Injectable, ParseIntPipe} from '@nestjs/common';
 import {lastValueFrom, map} from "rxjs";
+import {forEachResolvedProjectReference} from "ts-loader/dist/instances";
 
 @Injectable()
 export class AppService {
     constructor(private httpService: HttpService) {}
 
     apikey = 'd7601151be99cf3f0844d5dfc1f09e1f'
+    filialId = '66991b2a2f64b85e897c4b9c'
     url = 'https://api.telebon.ru/api'
-    async getUsers(): Promise<any> {
+
+    async getDates(employee_id): Promise<any> {
+        const dates = []
+
+        const datestart = `${new Date().getFullYear()}-`+
+        `${String(new Date().getMonth() + 1).padStart(2, '0')}-`+
+        `${String(new Date().getDate()).padStart(2, '0')}`
+
+        const dateend = `${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).getFullYear()}-`+
+        `${String(new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).getMonth() + 1).padStart(2, '0')}-`+
+        `${String(new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).getDate()).padStart(2, '0')}`
+        await lastValueFrom(this.httpService.post(`${this.url}/lessons/ulessons`,
+            {
+                apikey: this.apikey,
+                employee: employee_id,
+                datestart: datestart,
+                dateend: dateend
+            })
+            .pipe(
+                map(res => {
+                    if (!res.data.data) {
+                        return []
+                    }
+                    res.data.data.forEach(date => {
+                        const parts = date.date.split('-')
+                        dates.push(new Date(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2]), 12))
+                    })
+                })
+            )
+        )
+        return dates
+    }
+
+    async getTimes(employee_id, date){
+        let targetRecord :any = null
+        const datestart = `${new Date().getFullYear()}-`+
+            `${String(new Date().getMonth() + 1).padStart(2, '0')}-`+
+            `${String(new Date().getDate()).padStart(2, '0')}`
+
+        const dateend = `${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).getFullYear()}-`+
+            `${String(new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).getMonth() + 1).padStart(2, '0')}-`+
+            `${String(new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).getDate()).padStart(2, '0')}`
+
+        await lastValueFrom(this.httpService.post(`${this.url}/lessons/ulessons`,
+            {
+                apikey: this.apikey,
+                employee: employee_id,
+                datestart: datestart,
+                dateend: dateend
+            })
+            .pipe(
+                map(res => {
+                    const targetDateWithoutTime = new Date(new Date(date).getFullYear(), new Date(date).getMonth(), new Date(date).getDate());
+                    targetRecord = res.data.data.find(record => {
+                        const recordDate = new Date(record.date);
+                        const recordDateWithoutTime = new Date(recordDate.getFullYear(), recordDate.getMonth(), recordDate.getDate());
+                        return recordDateWithoutTime.getTime() === targetDateWithoutTime.getTime();
+                    });
+                })
+            )
+        )
+
+        return targetRecord
+    }
+
+    async getUsers(service_id): Promise<any> {
         return await lastValueFrom(this.httpService.post(`${this.url}/users`,
             {
                 apikey: this.apikey
             })
             .pipe(
-                map(res => res.data.users)
+                map(res => res.data.users
+                    .filter(user => user.services === true && user.idservices
+                        .some(service => service.idservice === service_id)))
             )
         );
     }
